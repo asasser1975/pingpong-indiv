10884b112b0fb85d96a5eb6a9cfdd78a6f1cda34
Student <example@example.com>
2019-03-11 20:37:45 -0700

    begin logic in js for loop of counter counting down to 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 // Business Logic
+var numberArray = [];
+//create loop counter down to 1
+var counter = 1;
+for (var i = n; i === 1; i--) {
+  counter = counter - i;
+}
+
 var pingPongResult = function(pingpong) {
   if (pingpong % 15 === 0) {
     return ;
@@ -10,3 +17,25 @@ var pingPongResult = function(pingpong) {
     return pingpong;
   }
 };
+
+// User Logic
+$(document).ready(function() {
+  $("form#factorials").submit(function(event) {
+    event.preventDefault();
+    var n = parseInt($("input#value").val());
+    console.log("user input", n);
+    var result = factorialsResult(n);
+    console.log("function result", result);
+    // $(".year").text(year);
+    //
+    // if (!result) {    // same as writing if (result ==== false)
+    //   $(".not").text("not");
+    // } else {
+    //   $(".not").text("");
+    // }
+
+
+    $("#result").text(result);
+    console.log(result);
+  });
+});
